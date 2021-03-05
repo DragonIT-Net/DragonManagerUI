a4bc2d3d03e1fb936558a65aa100ed95f22e01fc
dragon <467470001@qq.com>
2021-03-05 19:34:12 +0800

    第一次构建

---
+++ dragonMUI/src/views/dashboard/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <img
+      height="1000"
+      src="../../../image/C7181A414BCD2616311C70B0813F3134.png"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Dashboard",
+  computed: {
+    ...mapGetters(["name"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
